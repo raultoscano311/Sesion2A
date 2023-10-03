cf8ec2b604552439df6055ad5703de27e94c5f51
usuario <usuario@pc20-015>
2023-10-03 10:20:25 +0200

    commit

---
+++ src/cmepps/Calculadora.java
@@ -0,0 +1,23 @@
+package cmepps;
+
+public class Calculadora {
+	public static int suma(int a,int b) {
+		return a+b;
+	}
+	
+	public static int resta(int a,int b) {
+		return a-b;
+	}
+	
+	public static int divide(int a,int b) {
+		if(b==0) {
+			return -1;
+		}
+		else 
+			return a/b;
+	}
+	
+	public static int multiplica(int a,int b) {
+		return a*b;
+	}
+}
